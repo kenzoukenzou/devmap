658193de408f8fb94ec9982fe686e1c81baef354
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-06-12 22:15:35 +0900

    Tackle Issue #8 ( dynamic ogp )

--- functions/index.js
+++ functions/index.js
@@ -4,39 +4,44 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.pageOverview = functions.https.onRequest((req, res) => {
-  fs.readFile("index.html", "utf8", (e, html) => {
-    // req.path = '/spot/:overviewId'
+exports.returnHTMLWithOGP = functions.https.onRequest((req, res) => {
+  fs.readFile("./hosting/index.html", "utf-8", (e, html) => {
     const overviewId = req.path.split("/")[2];
     const firestore = admin.firestore();
-    // https://stackoverflow.com/questions/51441263/admin-sdk-cannot-set-settings-for-firestore
     try {
       firestore.settings({ timestampsInSnapshots: true });
     } catch (error) {
       // ignore
     }
     firestore
-      .doc(`/overviews/${overviewId}/`)
+      .collection("overviews")
+      .doc(overviewId)
       .get()
-      .then(overviewDoc => {
-        const overview = overviewDoc.exists ? overviewDoc.data() : null;
-        if (overview) {
-          html = html.replace(
-            html.match(/<title>.*<\/title>/),
-            `<title>Devmap - ${overview.title}</title>`
-          );
-
-          // html = html.replace(
-          //   html.match(/<meta name="og:title"[^>]*>/),
-          //   `<meta name="og:title" content="${overview.title}">`
-          // );
-          // html = html.replace(
-          //   html.match(/<meta name="og:image"[^>]*>/),
-          //   `<meta name="og:image" content="${overview.eyeCatchImg}">`
-          // );
-
-          res.set("Cache-Control", "public, max-age=3600, s-maxage=3600");
-        }
+      .then(doc => {
+        const overview = doc.data();
+        html = html.replace(
+          html.match(/<title>.*<\/title>/),
+          `<title>${overview.title} | ${
+            overview.authorName
+          }さんのロードマップ</title>`
+        );
+        html = html.replace(
+          html.match(/<meta property="og:image"[^>]*>/),
+          `<meta property="og:image" content="${overview.eyeCatchImg}">`
+        );
+        html = html.replace(
+          html.match(/<meta property="og:title"[^>]*>/),
+          `<meta property="og:title" content="${overview.title} | ${
+            overview.authorName
+          }さんのロードマップ">`
+        );
+        html = html.replace(
+          html.match(/<meta property="og:url"[^>]*>/),
+          `<meta property="og:url" content="https://devmap.work/overviews/${
+            overview.key
+          }">`
+        );
+        res.set("Cache-Control", "public, max-age=3600, s-maxage=3600");
         res.status(200).send(html);
         return overview;
       })
