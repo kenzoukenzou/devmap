5ef2805214bf4b3beedb98f1f8dca7b66b574905
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-06-28 11:36:30 +0900

    CicleCI_01

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import fbConfig from "./Firebase";
+import fbConfig from "./Firebase.js";
 import App from "./App";
 const store = createStore(
   rootReducer,
