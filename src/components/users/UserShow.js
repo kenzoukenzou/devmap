36bc41b16ce4104de545821e808c048fe4bed209
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-06-28 12:19:34 +0900

    CicleCI_07

---
+++ src/components/users/UserShow.js
@@ -0,0 +1,96 @@
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { Link } from "react-router-dom";
+import { unsubscribe } from "../../store/actions/authActions";
+import Helmet from "react-helmet";
+import Skeleton from "react-loading-skeleton";
+
+class UserShow extends Component {
+  onClick = e => {
+    // console.log(this.props.match.params.id);
+    this.props.unsubscribe(this.props.match.params.id);
+    this.props.history.push("/login");
+  };
+
+  render() {
+    const { user, overviews, auth } = this.props;
+    const overviewNodes =
+      overviews &&
+      overviews.map(overview => {
+        return overview.authorID === this.props.match.params.id ? (
+          <div className="overview_card clearfix">
+            <img
+              src={overview.eyeCatchImg}
+              alt="eyecatchImg"
+              className="float-left eyecatch"
+            />
+            <div className="float-left ml-5 mt-5 summary">
+              <h5>
+                <Link to={`/overviews/${overview.key}`}>{overview.title}</Link>
+              </h5>
+            </div>
+          </div>
+        ) : null;
+      });
+    let unsubscribeNodes;
+    if (auth.uid === this.props.match.params.id) {
+      unsubscribeNodes = (
+        <button className="btn btn-danger" onClick={this.onClick}>
+          退会する
+        </button>
+      );
+    }
+
+    return (
+      <Fragment>
+        <div className="text-center mt-5 mb-5">
+          {user ? (
+            <Fragment>
+              <img
+                style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+                src={user.avatarUrl}
+                alt=""
+              />
+              <h6 className="mt-1">{user.displayName}</h6>
+              {unsubscribeNodes}
+              <Helmet>
+                <title>{user.displayName}さんのプロフィール | Devmap</title>
+              </Helmet>
+            </Fragment>
+          ) : null}
+        </div>
+        {overviewNodes || (
+          <Skeleton className="wrapper" height={100} count={3} />
+        )}
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const users = state.firestore.data.users;
+  const user = users ? users[id] : null;
+
+  return {
+    overviews: state.firestore.ordered.overviews,
+    user: user,
+    auth: state.firebase.auth
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    unsubscribe: id => dispatch(unsubscribe(id))
+  };
+};
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  firestoreConnect([{ collection: "overviews" }])
+)(UserShow);
